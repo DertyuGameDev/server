f117be514fffca04e22e7ddb1f690b37fc4b5db3
DertyuDev <sidovidchenko@gmail.com>
2025-04-24 17:48:20 +0300

    123

--- server.py
+++ server.py
@@ -1,15 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    query_params = request.args.to_dict()
-    return jsonify({
-        "message": "Получены параметры",
-        "params": query_params
-    })
+    user_id = request.args.get("user_id")
+
+    # Простейшая авторизация
+    if user_id != "123":
+        return "⛔ Доступ запрещен. Неверный user_id.", 403
+
+    return render_template("index.html", user_id=user_id)
+
+
+@app.route('/swipe', methods=['POST'])
+def handle_swipe():
+    data = request.get_json()
+    user_id = data.get("user_id")
+    direction = data.get("direction")
+    print(f"Пользователь {user_id} свайпнул: {direction}")
+    return jsonify({"status": "ok", "received": direction})
 
 
 if __name__ == '__main__':
